4ab008a882879a473c596d1094ee93da4765d7a3
Andreea Moise <andreeam@topdesk.com>
2023-03-12 22:38:27 +0100

    Improve identification service to verify the whole object

--- swagger_server/service/identification_service.py
+++ swagger_server/service/identification_service.py
@@ -1,4 +1,5 @@
 import os
+# from flask import Flask
 import tempfile
 from functools import reduce
 
@@ -8,17 +9,38 @@ db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "identities.json")
 db = TinyDB(db_file_path)
 
+# app = Flask(__name__)
 
+
+# @app.route('/identify', methods=['POST'])
 def add(identification=None):
-    # queries = []
-    # query = Query()
-    # queries.append(query.first_name == student.first_name)
-    # queries.append(query.last_name == student.last_name)
-    # query = reduce(lambda a, b: a & b, queries)
-    res = db.search(identification)
-    # If entry exists in the db, then it is valid.
+    print(db_file_path)
+    queries = []
+    query = Query()
+
+    queries.append(query.truck_id == identification.truck_id)
+    queries.append(query.driver_id == identification.driver_id)
+    queries.append(query.location.latitude == identification.location.latitude)
+    queries.append(query.location.longitude == identification.location.longitude)
+
+    query = reduce(lambda a, b: a & b, queries)
+    res = db.search(query)
+
+    # Verify if the res contains also the correct items
+    # Assume only one result
+    sameItems = True
+
     if res:
-        return 'entry is valid', 200
+        rawItems = identification.load.items
+        resItems = res[0]['load']['items']
+
+        for x in range(len(rawItems)):
+            if rawItems[x].sku != resItems[x]['sku'] or rawItems[x].quantity != resItems[x]['quantity']:
+                sameItems = False
+
+         # If entry exists in the db, then it is valid.
+        if sameItems:
+            return 'entry is valid', 200
 
     db.insert(identification.to_dict())
     return 'entry is created', 201
