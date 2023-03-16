b1f1d80cd2e240fb150440e6e68ead4927ef2be6
Nikos Karampinas <nkarampi@gmail.com>
2023-03-16 19:43:01 +0100

    Added Dockerfile for BE

--- identification/server/README.md
+++ identification/server/README.md
@@ -1,1 +1,10 @@
 # Tracing API
+
+## Run
+- `npm i`
+- `npm start`
+
+## Run with Docker
+- Install Docker
+- `docker build . -t tracking/server`
+- `docker run --env PORT=8080 -p 8080:8080 -d tracking/server`
--- identification/server/index.js
+++ identification/server/index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/tracking.routes');
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
